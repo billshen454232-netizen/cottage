a846fab15210c99503f29f90d5da175d94260b41
v_shentao <v_shentao@bilibili.com>
2026-06-02 16:21:06 +0800

    first commit

---
+++ src/data/services.ts
@@ -0,0 +1,22 @@
+export const services = [
+  {
+    name: '订阅门户',
+    url: 'https://subs.ttlink.asia/subs.html',
+    status: '已上线',
+  },
+  {
+    name: 'CLI Proxy API',
+    url: 'https://cli.ttlink.asia/management.html',
+    status: '已上线',
+  },
+  {
+    name: '个人博客',
+    url: 'https://blog.ttlink.asia',
+    status: '计划中',
+  },
+  {
+    name: '知识图谱',
+    url: 'https://graph.ttlink.asia',
+    status: '计划中',
+  },
+];
